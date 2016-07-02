00329d02ddfec94613a302c25f0f9fa8824f2824
Supason Kotanut <supasonk@gmail.com>
2016-07-02 22:15:02 +0300

    add object creation code

---
+++ OldBirds2.4/Assets/Scripts/Management Panel/ManagementPanel.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManagementPanel : MonoBehaviour {
+
+	bool creating;
+	public GameObject start;
+	public GameObject end;
+	public GameObject sphere;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		getInput ();
+
+	}
+	void getInput () {
+		if (Input.GetMouseButtonDown (0)) {
+			//setStart ();
+			createBall();
+		} else if ( Input.GetMouseButton(0)) {
+			setBallPosition ();
+		}
+		else if (Input.GetMouseButtonUp(0)) {
+			//setEnd ();
+
+		} //else {
+		//if (creating) {
+		//	adjust ();
+		//}
+		//}
+	}
+	void createBall(){
+		sphere = Instantiate (sphere, getWorldPoint(), Quaternion.identity) as GameObject;
+	}
+	void setBallPosition(){
+		sphere.transform.position = getWorldPoint ();
+	}
+	void setStart (){
+		creating = true;
+		start.transform.position = getWorldPoint ();
+	}
+	void setEnd (){
+		creating = false;
+		end.transform.position = getWorldPoint ();
+	}
+	void adjust (){
+
+	}
+	Vector3 getWorldPoint(){
+		Ray ray = GetComponent<Camera>().ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		if(Physics.Raycast(ray, out hit)){
+			return hit.point;
+		}
+		return Vector3.zero;
+	}
+}
