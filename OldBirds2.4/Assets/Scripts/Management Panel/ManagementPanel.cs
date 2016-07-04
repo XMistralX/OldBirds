81c8fa1ae29959bc0c6b721374123114ce92cfc2
Sorrawit Chancherngkit <ongoing@outlook.com>
2016-07-04 03:43:32 +0300

    can edit created object's position by select and drag. Edit unity setting to compatible with git workflow

--- OldBirds2.4/Assets/Scripts/Management Panel/ManagementPanel.cs
+++ OldBirds2.4/Assets/Scripts/Management Panel/ManagementPanel.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class ManagementPanel : MonoBehaviour {
 
-	bool creating;
+	public Shader silhouette;
+	private ArrayList selectedBirds;
+	private GameObject[] selectedBirdObjects;
+
+	bool isCreating;
+
 	public GameObject start;
 	public GameObject end;
 	public GameObject sphere;
+	private GameObject selectedObject;
 	// Use this for initialization
 	void Start () {
-
+		selectedBirds = new ArrayList();
 	}
 
 	// Update is called once per frame
@@ -19,13 +25,14 @@ public class ManagementPanel : MonoBehaviour {
 	}
 	void getInput () {
 		if (Input.GetMouseButtonDown (0)) {
-			//setStart ();
-			createBall();
+			handleSelection();
 		} else if ( Input.GetMouseButton(0)) {
-			setBallPosition ();
+			if (isCreating) {
+				setSelectedObjectPosition ();
+			}
 		}
 		else if (Input.GetMouseButtonUp(0)) {
-			//setEnd ();
+			setEnd ();
 
 		} //else {
 		//if (creating) {
@@ -34,22 +41,79 @@ public class ManagementPanel : MonoBehaviour {
 		//}
 	}
 	void createBall(){
-		sphere = Instantiate (sphere, getWorldPoint(), Quaternion.identity) as GameObject;
+		selectedObject = Instantiate (sphere, getWorldPoint(), Quaternion.identity) as GameObject;
 	}
-	void setBallPosition(){
-		sphere.transform.position = getWorldPoint ();
+	void setSelectedObjectPosition(){
+		selectedObject.transform.position = getWorldPoint ();
 	}
 	void setStart (){
-		creating = true;
-		start.transform.position = getWorldPoint ();
+		isCreating = true;
 	}
 	void setEnd (){
-		creating = false;
-		end.transform.position = getWorldPoint ();
+		isCreating = false;
 	}
 	void adjust (){
 
 	}
+
+	void handleSelection() {
+		Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+		RaycastHit hit;
+		if( Physics.Raycast( ray, out hit, 2000 ) )
+		{
+			// Accept only transforms tagged with "Bird"
+			if (hit.transform.tag == "Bird") {
+				selectBird (hit);
+			} else if (hit.transform.tag == "ManagementObject") {
+				selectedObject = hit.transform.gameObject;
+				setStart ();
+			} else {
+				setStart ();
+				createBall ();
+			}
+
+		}
+	}
+
+	void selectBird(RaycastHit hit) {
+		// Check if gameobject found in list already
+		bool found = false;
+		foreach(GameObject selection in selectedBirds) {
+			if(selection == hit.transform.gameObject) {
+				found = true;
+			}
+		}
+
+		// Close info window
+		updateBirdList();
+		foreach(GameObject bird in selectedBirdObjects) {
+			bird.GetComponent<InformationTextScript>().setShow(false);
+		}
+
+		if(!found) {
+			// Add to list and change shader for the silhouette
+			selectedBirds.Add(hit.transform.gameObject);
+			foreach(Transform child in hit.transform) {
+				if(child.name == "Body") {
+					// Add silhouette only to the body object
+					child.GetComponent<Renderer>().material.shader = silhouette;
+					child.GetComponent<Renderer>().material.SetColor ("_OutlineColor", Color.green);
+				}
+			}
+		} else {
+			// Remove from list and remove silhouette
+			selectedBirds.Remove(hit.transform.gameObject);
+			foreach(Transform child in hit.transform) {
+				if(child.name == "Body") {
+					child.GetComponent<Renderer>().material.shader = Shader.Find ("Diffuse");
+
+				}
+			}
+		}
+
+		found = false;
+	}
+
 	Vector3 getWorldPoint(){
 		Ray ray = GetComponent<Camera>().ScreenPointToRay (Input.mousePosition);
 		RaycastHit hit;
@@ -57,5 +121,34 @@ public class ManagementPanel : MonoBehaviour {
 			return hit.point;
 		}
 		return Vector3.zero;
+	}
+
+	public ArrayList getSelectionList() {
+		return this.selectedBirds;
+	}
+
+	public void addToSelectionList(GameObject bird) {
+		selectedBirds.Add(bird);
+		foreach(Transform child in bird.transform) {
+			if(child.name == "Body") {
+				child.GetComponent<Renderer>().material.shader = silhouette;
+				child.GetComponent<Renderer>().material.SetColor ("_OutlineColor", Color.green);
+			}
+		}
+	}
+
+	public void updateBirdList() {
+		selectedBirdObjects = new GameObject[selectedBirds.Count];
+		for(int i = 0; i < selectedBirds.Count; i++) {
+			selectedBirdObjects[i] = (GameObject)selectedBirds.ToArray()[i];
+		}
+	}
+
+	public void showInfoWindow() {
+		updateBirdList();
+
+		foreach(GameObject bird in selectedBirdObjects) {
+			bird.GetComponent<InformationTextScript>().show();
+		}
 	}
 }
