7fb1642484d02e31d976644681e4ac0099c04a4a
Supason Kotatnut <supasonk@gmail.com>
2016-07-25 16:25:26 +0300

    add some ui code

---
+++ OldBirds2.4/Assets/Scripts/Management Panel/UIControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIControl : MonoBehaviour {
+
+	private GameObject holder;
+	private GameObject birds;
+	// Use this for initialization
+	void Start () {
+		holder = GameObject.Find("Text");
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		holder.GetComponent<RectTransform> ().anchoredPosition3D = Input.mousePosition;
+		print (Input.mousePosition);
+	}
+}
