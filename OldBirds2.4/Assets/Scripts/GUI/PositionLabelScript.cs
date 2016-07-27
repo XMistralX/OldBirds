ee7eb435cd0e8daf4c0c65cf7f8c5415e08fa5dd
Supason Kotanut <supasonk@gmail.com>
2016-07-27 23:22:21 +0300

    show selected object position , rotation and scale. add delete object button

---
+++ OldBirds2.4/Assets/Scripts/GUI/PositionLabelScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PositionLabelScript : MonoBehaviour {
+
+	// Use this for initialization
+	private GameObject holder;
+	private ManagementPanel managementScript;
+
+	Text Position;
+
+	void Start () {
+		holder = GameObject.Find("MainController");
+		managementScript = holder.GetComponent<ManagementPanel> ();
+		Position = gameObject.GetComponent<Text> ();
+		Position.text = managementScript.getObjectLocation ().ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Position.text = managementScript.getObjectLocation ().ToString();
+	}
+}
---
+++ OldBirds2.4/Assets/Scripts/GUI/RotationLabelScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RotationLabelScript : MonoBehaviour {
+
+	// Use this for initialization
+	private GameObject holder;
+	private ManagementPanel managementScript;
+
+	Text Rotation;
+
+	void Start () {
+		holder = GameObject.Find("MainController");
+		managementScript = holder.GetComponent<ManagementPanel> ();
+		Rotation = gameObject.GetComponent<Text> ();
+		Rotation.text = managementScript.getObjectRotation ().ToString();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Rotation.text = managementScript.getObjectRotation ().ToString();
+	}
+}
